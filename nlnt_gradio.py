05a2a20cdc9436920c874f7d31d9111d52992b09
marae <mara.escano123@gmail.com>
2024-04-18 12:28:16 +0800

    subprocess

--- nlnt_gradio.py
+++ nlnt_gradio.py
@@ -1,6 +1,8 @@
+# Gazebo & SSH Quickstart Connection to Turtlebot
+
 ## with CSS & livestreaming (WORKS !)
-# documentation for ASR Demo with Transformers : https://www.gradio.app/guides/real-time-speech-recognition
 # documentation for livestreaming: https://www.gradio.app/guides/reactive-interfaces
+# should add connection to Turtlebot
 
 import gradio as gr
 from transformers import pipeline
@@ -10,9 +12,17 @@ import json
 import ast
 from knetworking import DataBridgeServer_TCP
 from collections import deque
+
 import subprocess
 
+import gradio as gr
+from transformers import pipeline
+import numpy as np
+
 launch_demo_ttb = subprocess.Popen('python3 ~/demo-lv1-lv2/demo_ttb.py', stdout=subprocess.DEVNULL, shell=True)
+
+print('Waiting for Turtlbot connection...')
+server = DataBridgeServer_TCP()
 
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
 
@@ -20,9 +30,6 @@ theme = gr.themes.Default(primary_hue= gr.themes.colors.emerald, secondary_hue=g
     button_primary_background_fill="*primary_200",
     button_primary_background_fill_hover="*primary_200",
 )
-
-print('Waiting for Turtlbot connection...')
-server = DataBridgeServer_TCP()
 
 css = """
 .color_btn textarea  {background-color: #228B22; !important}
@@ -36,13 +43,9 @@ def transcribe(audio):
     return transcriber({"sampling_rate": sr, "raw": y})["text"]
 
 def nlnt (vid_check, prompt, video, history="None", progress=gr.Progress()):
-    progress(0, desc="Starting...")
-
-    if vid_check == True:
-        return level3_model(prompt, video)
-    else:
-
-        server.send_data('START')
+  if vid_check == True:
+      return level3_model(prompt, video)
+  else:
 
         # Level 2 Model
         x_dict = ast.literal_eval(main(prompt))
@@ -51,16 +54,6 @@ def nlnt (vid_check, prompt, video, history="None", progress=gr.Progress()):
 
         state_number = 0
         while x_dict["instruction complete"] == "#ongoing":       # JSON bug here where it doesn't recognize dictionary 
-            
-            #while "#ongoing" in x:
-            # send x to Turtlebot using TCP => Turtlebot churva
-            # send json string to Turtlebot
-            # send x_dict["movement message"]
-            # receive y from turtlebot using TCP
-            # x_dict["orientation"] = actual_orientation
-            # x_dict["distance to next point"] = actual_distance
-            # x_dict["execution length"] = actual_execution length
-            # send json string back and append to history
             # {
             #   "state number": "0x2", 
             #   "orientation": 1.29, 
@@ -72,7 +65,9 @@ def nlnt (vid_check, prompt, video, history="None", progress=gr.Progress()):
             
             # x_dict is previous state;
 
-            lin_x, ang_z = x_dict['movement_message']
+            server.send_data('START')
+
+            lin_x, ang_z = x_dict['movement message']
             dt = x_dict['execution length']
             mess = str([lin_x, ang_z, dt, 0])
 
@@ -90,11 +85,10 @@ def nlnt (vid_check, prompt, video, history="None", progress=gr.Progress()):
             x = main(prompt, history)
             x_dict = ast.literal_eval(x)
             state_number += 1
-            
-        return x
+        return "Done!"
 
-def level3_model (prompt, video):
-    return "level 3: " + prompt
+def level3_model (textbox, ext_video, rpi_video):
+    return "level 3: " + textbox
 
 def show_vid (vid_check):
     if vid_check:
@@ -102,34 +96,65 @@ def show_vid (vid_check):
     else:
       return gr.update(visible=False)
 
+def connecting (connect_to, gz_world, ttb_ip):
+    if connect_to == "Gazebo":
+        launch_gazebo = subprocess.Popen('export TURTLEBOT3_MODEL=burger ; ros2 launch turtlebot3_gazebo empty_world.launch.py', stdout=subprocess.DEVNULL, shell=True)
+        return connect_to_gazebo(gz_world)
+    else:
+        return connect_to_ttb(ttb_ip)
+
+def connect_to_gazebo(gz_world):
+  # quickstart here
+  return "Connected to Gazebo"
+
+def connect_to_ttb(ttb_ip):
+  # quickstart here
+  return "Connected to Turtlebot@" + ttb_ip
+
+def launch(commands):
+    launcher = subprocess.Popen(commands, shell=True, text = True)
+    
+    if commands[0:1] == 'ls':
+        print(launcher.stdout)
+
 with gr.Blocks(theme=theme, css=css, title = "NLNT Demo") as demo:
     gr.Markdown(
     """
     # Natural Language Ninja Turtle
     A Natural Language to ROS2 Translator for the Turtlebot V3 Burger
-    """)
-    with gr.Row():
+    """, elem_id = "title")
+    with gr.Tab('Connect'):
+      with gr.Row():
+        # Gazebo, SSH
+        connect_to = gr.Dropdown(label = "Connect to", choices = ["Gazebo", "Actual Turtlebot"], value = "Gazebo")
+      with gr.Row():
+        gz_world = gr.Textbox(label = "Gazebo World", placeholder = "turtlebot3_empty_world", interactive = True, visible = True)
+        ttb_ip = gr.Textbox(label = "Turtlebot IP", interactive = True, visible = True)
+      with gr.Row():
+        connect_btn = gr.Button(value = "Connect", elem_classes = "color_btn")
+      with gr.Row():
+        con_status = gr.Textbox(label = "Connection Status", placeholder = "Not Connected.", interactive = False)
+        connect = connect_btn.click(fn=connecting, inputs=[connect_to, gz_world, ttb_ip], outputs=con_status)
+    with gr.Tab('NLNT'):
+      with gr.Row():
         vid_check = gr.Checkbox(label = "connect live video")
-    with gr.Row(equal_height=True):
+      with gr.Row(equal_height=True):
         audio = gr.Audio(sources=["microphone"])
         prompt = gr.Textbox(label = "Instruction", placeholder = "move 1.5 meters forward", interactive = True)
-    with gr.Row():
+      with gr.Row():
         clr_audio = gr.ClearButton(value = "clear audio", components = [audio])
         transcribe_btn = gr.Button(value = "Transcribe", elem_classes = "color_btn")
         clr_text = gr.ClearButton(value = "clear text", components = [audio, prompt])
         transcription = transcribe_btn.click(fn=transcribe, inputs=audio, outputs=prompt)
-        #transcription = gr.Interface(transcribe, audio, prompt)
-    with gr.Row():
-      ttbt_btn = gr.Button(value = "Run Instruction", elem_classes = "color_btn")
-    with gr.Row():
+      with gr.Row():
+        ttbt_btn = gr.Button(value = "Run Instruction", elem_classes = "color_btn")
+      with gr.Row():
         video = gr.Image(sources=["webcam"], streaming=True, visible=False)
         ckbx = vid_check.select(fn = show_vid, inputs = vid_check, outputs = video)
-        #vid_check.change(show_vid, vid_check, video)
-    with gr.Row():
-      with gr.Column():
-        status = gr.Textbox(label = "Status", placeholder = "Please enter your prompt.")
-        #history = None
-        run_nlnt = ttbt_btn.click(fn=nlnt, inputs=[vid_check, prompt, video], outputs=status)
+      with gr.Row():
+        with gr.Column():
+          status = gr.Textbox(label = "Status", placeholder = "Please enter your prompt.")
+          run_nlnt = ttbt_btn.click(fn=nlnt, inputs=[vid_check, prompt, video], outputs=status)
 
 
 demo.launch()
