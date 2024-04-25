680426cd870c6fd44617dbeb3238d36ca851d727
marae <mara.escano123@gmail.com>
2024-04-25 15:42:50 +0800

    prints each state in gradio while inferencing

--- inference_gradio.py
+++ inference_gradio.py
@@ -33,7 +33,6 @@ def main(prompt, history="None"):
     data = {'content': prompt}
 
     try:
-        #print(chalk.magenta("sending prompt to inference server..."))
         start_time = time.time()
 
         response = requests.post(url, json=data, headers=headers)
@@ -41,8 +40,6 @@ def main(prompt, history="None"):
 
         end_time = time.time()
         elapsed_time = end_time - start_time
-        #print(chalk.magenta("response received!"),
-        #      f'{round(elapsed_time, 2)} seconds')
 
         json_object = response.json()
         json_formatted_str = json.dumps(json_object, indent=2)
@@ -51,30 +48,12 @@ def main(prompt, history="None"):
         json_end = json_formatted_str.rfind('</s>') - 1
 
         to_return = json_formatted_str[json_start:json_end].replace("'", '"').strip()
-        #print(to_return)
-        #print(json_formatted_str)
-        #json_response = json.loads(to_return)
-        #print(json_response)
-        #print(json_response["instruction complete"])
-        #print(json_response["generated"])
-        #gen = json_response["generated"]
-        #split_json = gen.split('### Answer: ')
-        #print(split_json[-1])
-        #returned = split_json[-1].replace("</s>", "")
-        #print(returned)
-        #ret_json = json.dumps(returned, indent=2)
-        #ret = "{'state number': '0x0', 'orientation': 0.0, 'distance to next point': 0.82, 'execution length': 4.208, 'movement message': (0.2, 0.0), 'instruction complete': '#ongoing'}"
-        #ret_json = json.dumps(returned, indent=2)
-        #ret_dt = json.loads(ret_json)
-        #print(ret_dt)
-        #print(ret_dt["instruction complete"])
         
         return to_return
     
     except requests.RequestException as e:
         print("Error:", e)
-        return "Error: " + e
-        #return None
+        return
 
     return
 
