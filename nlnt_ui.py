297e1e259a464c5d384b62f065d056da15670d8c
reddiedev <105657266+reddiedev@users.noreply.github.com>
2024-05-07 19:08:40 +0800

    maaraaaaawr

--- nlnt_ui.py
+++ nlnt_ui.py
@@ -14,48 +14,32 @@ import os
 
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
 
-theme = gr.themes.Default(primary_hue= gr.themes.colors.emerald, secondary_hue=gr.themes.colors.slate, neutral_hue=gr.themes.colors.slate).set(
-    button_primary_background_fill="*primary_200",
-    button_primary_background_fill_hover="*primary_200",
+theme = gr.themes.Monochrome(
+    primary_hue="green",
+    secondary_hue="green",
+    neutral_hue="stone",
+    font=[gr.themes.GoogleFont('Jersey 25 Charted '), gr.themes.GoogleFont('exo-2'), 'system-ui', 'sans-serif'],
+).set(
+    button_small_radius='*radius_sm',
+    button_primary_background_fill='*secondary_800',
+    button_primary_background_fill_dark='*neutral_400',
+    button_primary_border_color_dark='*secondary_800',
+    button_primary_text_color_dark='*neutral_800',
+    button_primary_text_color_hover='*neutral_50',
+    button_primary_text_color_hover_dark='*neutral_50',
+    button_secondary_background_fill_dark='*neutral_400',
+    button_secondary_background_fill_hover='*neutral_800',
+    button_secondary_background_fill_hover_dark='*button_primary_background_fill_hover',
+    button_secondary_text_color='*neutral_50',
+    button_secondary_text_color_dark='*neutral_800',
+    button_secondary_text_color_hover='*button_secondary_border_color_hover',
+    button_secondary_text_color_hover_dark='*neutral_50'
 )
 
 css = """
 .color_btn textarea  {background-color: #228B22; !important}
 """
 
-js = """
-function createGradioAnimation() {
-    var container = document.createElement('div');
-    container.id = 'gradio-animation';
-    container.style.fontSize = '2em';
-    container.style.fontWeight = 'bold';
-    container.style.textAlign = 'center';
-    container.style.marginBottom = '20px';
-
-    var text = 'Welcome to Gradio!';
-    for (var i = 0; i < text.length; i++) {
-        (function(i){
-            setTimeout(function(){
-                var letter = document.createElement('span');
-                letter.style.opacity = '0';
-                letter.style.transition = 'opacity 0.5s';
-                letter.innerText = text[i];
-
-                container.appendChild(letter);
-
-                setTimeout(function() {
-                    letter.style.opacity = '1';
-                }, 50);
-            }, i * 250);
-        })(i);
-    }
-
-    var gradioContainer = document.querySelector('.gradio-container');
-    gradioContainer.insertBefore(container, gradioContainer.firstChild);
-
-    return 'Animation created';
-}
-"""
 #print('Waiting for Turtlbot connection...')
 #ttb_script_path = os.path.join(os.getcwd(),"demo_ttb.py")
 #launch_demo_ttb = subprocess.Popen(f'python3 {ttb_script_path}', stdout=subprocess.DEVNULL, shell=True)
@@ -79,9 +63,15 @@ def nlnt (vid_check, prompt, video, history="None", progress=gr.Progress()):
 
 def level2_model(prompt, history="None", progress=gr.Progress()):
     progress(0, desc="Starting...")
-
     server.send_data('START')
-        
+    
+    # new output format
+    
+    x = main(prompt, "None")                          # initial prompt ; returns whether prompt is possible or not
+    
+    # if impossible return: "Action impossible. Please try again."
+    # else go to while loop
+    
     history = deque([])
     state_number = 0
         
@@ -89,14 +79,10 @@ def level2_model(prompt, history="None", progress=gr.Progress()):
     i = 0
 
     while x_dict["instruction complete"] == "#ongoing":
-        
-        if i != 11:
-            i += 1 
-  
         if history != deque([]):
-            x = main(prompt, [i for i in history])
+            x = main(prompt, [i for i in history])                  # send prompt to lambda server
         else:
-            x = main(prompt, "None")
+            x = main(prompt, "None")                                # send prompt to lambda server
 
         x_dict = ast.literal_eval(x)
 
@@ -104,13 +90,20 @@ def level2_model(prompt, history="None", progress=gr.Progress()):
         lin_x, ang_z = x_dict['movement message']
         dt = x_dict['execution length']
         code = 1 if x_dict['instruction complete'] == '#complete' else 0
+        expected_states = x_dict['expected number of states']               # TODO: double chack with actual output
 
-        progress(i/12, desc=f'Ongoing... Next Action: ({str(lin_x)}, {str(ang_z)}, {str(dt)})')
+        if i != expected_states:
+            i += 1
+            status = i/expected_states
+        else:
+            status = 0.99
+
+        progress(status, desc=f'Ongoing... Next Action: ({str(lin_x)}, {str(ang_z)}, {str(dt)})')
 
         mess = str([lin_x, ang_z, dt, code])
 
-        server.send_data(mess.encode())
-        data = ast.literal_eval(server.receive_data().decode())
+        server.send_data(mess.encode())                                     # send the predicted action to turtlebot
+        data = ast.literal_eval(server.receive_data().decode())             # receive actual action from turtlebot
 
         x_dict['state number'] = hex(state_number)
         x_dict['orientation'] = data['orientation']
@@ -118,7 +111,7 @@ def level2_model(prompt, history="None", progress=gr.Progress()):
 
         history.append(str(x_dict))
         if len(history) > 5:
-           history.popleft()
+            history.popleft()
             
         print(history[-1])
         print('\n')
@@ -139,12 +132,12 @@ def show_vid (vid_check):
 
 # js=
 with gr.Blocks(theme=theme, css=css, title = "NLNT Demo",js="metadata.js") as demo:
-    
-    gr.Markdown(
-    """
-    # Natural Language Ninja Turtle
-    A Natural Language to ROS2 Translator for the Turtlebot V3 Burger
-    """)
+    with gr.Row():
+        gr.Markdown(
+        """
+        # Natural Language Ninja Turtle
+        A Natural Language to ROS2 Translator for the Turtlebot V3 Burger
+        """)
     with gr.Row():
         vid_check = gr.Checkbox(label = "connect live video")
     with gr.Row(equal_height=True):
@@ -157,16 +150,16 @@ with gr.Blocks(theme=theme, css=css, title = "NLNT Demo",js="metadata.js") as de
         transcription = transcribe_btn.click(fn=transcribe, inputs=audio, outputs=prompt)
         #transcription = gr.Interface(transcribe, audio, prompt)
     with gr.Row():
-      ttbt_btn = gr.Button(value = "Run Instruction", elem_classes = "color_btn")
+        ttbt_btn = gr.Button(value = "Run Instruction", elem_classes = "color_btn")
     with gr.Row():
         video = gr.Image(sources=["webcam"], streaming=True, visible=False)
         ckbx = vid_check.select(fn = show_vid, inputs = vid_check, outputs = video)
         #vid_check.change(show_vid, vid_check, video)
     with gr.Row():
-      with gr.Column():
-        status = gr.Textbox(label = "Status", placeholder = "Please enter your prompt.")
-        #history = None
-        run_nlnt = ttbt_btn.click(fn=nlnt, inputs=[vid_check, prompt, video], outputs=status)
+        with gr.Column():
+            status = gr.Textbox(label = "Status", placeholder = "Please enter your prompt.")
+            #history = None
+            run_nlnt = ttbt_btn.click(fn=nlnt, inputs=[vid_check, prompt, video], outputs=status)
     with gr.Row():
         gr.HTML("""
         <div style='height: 30px; width: 100%;'>
