81144303c7ebb725d15758f407583f17bef6ddde
reddiedev <105657266+reddiedev@users.noreply.github.com>
2024-05-10 11:43:45 +0800

    added batt percentage

--- metadata.js
+++ metadata.js
@@ -2,6 +2,7 @@ function createGradioAnimation() {
     dist = document.querySelector('#total-dist');
     degs = document.querySelector('#total-degs');
     breakdown = document.querySelector('#prompt');
+    batt = document.querySelector('#batt');
     timeout_ms = 500
     setInterval(() => {
         fetch('http://127.0.0.1:8000/metadata')
@@ -15,6 +16,7 @@ function createGradioAnimation() {
                 dist.innerHTML = `${data.total_distance_traveled}`
                 degs.innerHTML = `${data.total_degrees_rotated}°`
                 breakdown.innerHTML = `${data.prompt}`
+                batt.innerHTML = `${data.battery_percentage}`
             })
             .catch(error => {
                 console.error('There was a problem with the fetch operation:', error);
